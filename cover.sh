d7599bce72450b750707b8bf47ee20e8726268fc
Hasit Mistry <hasit@fluxninja.com>
2023-05-30 14:08:31 -0700

    Update cover.sh file

--- cover.sh
+++ cover.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# Create a temporary file for coverage output
 TMP=$(mktemp /tmp/olric-coverage-XXXXX.txt)
 
-BUILD=$1
-OUT=$2
+# Set the output file name
+OUT=$1
 
+# Exit immediately if a command exits with a non-zero status
 set -e
 
-# create coverage output
-echo 'mode: atomic' > $OUT
-for PKG in $(go list ./...| grep -v -E 'vendor'|grep -v -E 'hasher'|grep -v -E 'internal/bufpool'|
-grep -v -E 'internal/flog'|grep -v -E 'serializer'|grep -v -E 'stats'|grep -v -E 'cmd'); do
-  go test -covermode=atomic -coverprofile=$TMP $PKG
-  tail -n +2 $TMP >> $OUT
-done
+# Initialize the coverage output file
+echo 'mode: atomic' > "$OUT"
 
+# Iterate through all packages, excluding specific ones, and run tests with coverage
+for PKG in $(go list ./... | grep -v -E 'vendor|hasher|internal/bufpool|internal/flog|serializer|stats|cmd'); do
+  go test -covermode=atomic -coverprofile="$TMP" "$PKG"
+  
+  # Append the coverage results to the output file, skipping the first line (header)
+  tail -n +2 "$TMP" >> "$OUT"
+done
